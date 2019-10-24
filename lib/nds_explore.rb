6e94f456a2b137a24bdba39349bf464a0131b848
Michael Espina <michael_espina@yahoo.com>
2019-10-24 02:52:29 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -1,3 +1,4 @@
+require 'pry'
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
 # Call the method directors_database to retrieve the NDS
@@ -9,6 +10,7 @@ end
 
 def print_first_directors_movie_titles
   column_index = 0
+  binding.pry
   if directors_database[0][:name] =="Stephen Spielberg"
       while column_index < directors_database.select{|k,v|k == :title}.count do
             puts "#{directors_database[0][:movie][0][:title]}\n"
@@ -16,3 +18,4 @@ def print_first_directors_movie_titles
       end
     end
 end
+
