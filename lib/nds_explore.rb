bac1b92c6c5bb097349aa8177d6b71c7c0cb64be
Michael Espina <michael_espina@yahoo.com>
2019-10-24 01:59:11 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -11,5 +11,8 @@ def print_first_directors_movie_titles
   column_index = 0
   if directors_database[:movie] =="Stephen Spielberg"
       while column_index < directors_database[:movie][:title].length do
-            puts "#{"
+            puts "#{directors_database[:movie][:title}\n"
+            column_index+= 1
+      end
+    end
 end
